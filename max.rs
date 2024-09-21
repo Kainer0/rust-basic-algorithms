e031308c7527d6a266381e3465a2a3a847156691
Kainer0 <kevin.cainero02@gmail.com>
2024-09-21 19:51:38 +0200

    max_array to modular

--- max.rs
+++ max.rs
@@ -1,15 +1,21 @@
 fn main() {
-    let array=[1,4,3,2,2];
-    let mut max:i32;
+    let array = [1, 4, 3, 18, 2];
     
-    for i in array {
-        println!("{}",i);
-        if i>max {
-            max=i;
+    let max = max(array);
+    
+    println!("Il valore più grande è: {}", max);
+    println!("{:?}", array);
+}
+
+fn max<const N: usize>(array: [i32; N]) -> i32 {
+    let mut max: i32 = array[0];
+    
+    for &i in array.iter() {
+        println!("{}", i);
+        if i > max {
+            max = i;
         }
     }
     
-    println!("il valore piu' grande e': {}",max);
-    println!("{:?}",array);
-    
+    return max;
 }
